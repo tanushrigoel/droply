a2a82bf99198ac2983022666c228e3ec5153b1f8
Tanushri Goel <tanushri@lpu.in>
2025-08-26 13:23:00 +0530

    pushing code

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import SignInForm from "@/components/signin";
+
+export default function SignInPage() {
+  return (
+    <div>
+      <SignInForm />
+    </div>
+  );
+}
---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import SignUpForm from "@/components/signup";
+
+export default function SignupPage(){
+    return <div>
+        <SignUpForm/>
+    </div>
+}
